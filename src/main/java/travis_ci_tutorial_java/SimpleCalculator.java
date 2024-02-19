9d4be1295716331b6f9bbd9dc9236215ad73e070
mac <mac@10.0.0.170>
2024-02-18 21:35:21 -0500

    Fifth Commit

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -7,8 +7,12 @@ public class SimpleCalculator {
 		public int subtract(int a, int b) {
 			return a - b;
 		}
-	/*
-	 * public int subtract(int a, int b) { return a - b; } public int multiply(int
-	 * a, int b) { return a * b; } public int divide(int a, int b) { return a / b; }
-	 */
+		
+		public int multiply(int a, int b) {
+			return a * b;
+		}
+		
+		public int divide(int a, int b) {
+			return a / b;
+		}
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -16,4 +16,22 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.subtract(5, 3), 2);
 	}
+	
+	@Test
+	public void testmultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+	}
+	
+	@Test
+	public void testdivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(9, 3), 3);
+	}
+	
+	@Test
+	public void testmultiplyAnother() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(8, 0), 0);
+	}
 }
