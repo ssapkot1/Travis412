95d1dc3490679d2879543ddbca3b00b1b7dfd79b
mac <mac@10.0.0.170>
2024-02-18 21:16:05 -0500

    Fourth commit

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,6 +4,9 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+		public int subtract(int a, int b) {
+			return a - b;
+		}
 	/*
 	 * public int subtract(int a, int b) { return a - b; } public int multiply(int
 	 * a, int b) { return a * b; } public int divide(int a, int b) { return a / b; }
